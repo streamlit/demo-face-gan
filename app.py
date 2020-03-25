513fd1c0febe2439811cb209c730aef5c15eb12a
tc87 <tcricks87@gmail.com>
2020-03-25 16:13:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,34 +21,34 @@ TL_GAN_HASH_FUNCS = {
 
 def main():
     st.title("Streamlit Face-GAN Demo")
-    # Download all data files if they aren't already in the working directory
+    # Download all data files if they aren't already in the working directory.
     for filename in EXTERNAL_DEPENDENCIES.keys():
         download_file(filename)
 
-    # Read in models from the data files
+    # Read in models from the data files.
     tl_gan_model, feature_names = load_tl_gan_model()
     session, pg_gan_model = load_pg_gan_model()
 
     st.sidebar.title('Features')
     seed = 27834096
-    # If the user doesn't want to select which features to control, these will be used
+    # If the user doesn't want to select which features to control, these will be used.
     default_control_features = ['Young','Smiling','Male']
     if st.sidebar.checkbox('Show advanced options'):
-        # Randomly initialize feature values 
+        # Randomly initialize feature values. 
         features = get_random_features(feature_names, seed)
-        # Let the user pick which features to control with sliders
+        # Let the user pick which features to control with sliders.
         control_features = st.sidebar.multiselect( 'Control which features?',
             sorted(features), default_control_features)
     else:
         features = get_random_features(feature_names, seed)
-        # Don't let the user pick feature values to control
+        # Don't let the user pick feature values to control.
         control_features = default_control_features
     
-    # Insert user-controlled values from sliders into the feature vector
+    # Insert user-controlled values from sliders into the feature vector.
     for feature in control_features:
         features[feature] = st.sidebar.slider(feature, 0, 100, 50, 5)
 
-    # Generate a new image from this feature vector (or retrieve it from the cache)
+    # Generate a new image from this feature vector (or retrieve it from the cache).
     image_out = generate_image(session, pg_gan_model, tl_gan_model,
             features, feature_names)
     st.image(image_out, use_column_width=True)
@@ -90,24 +90,24 @@ def download_file(file_path):
         if progress_bar is not None:
             progress_bar.empty()
 
-# Ensure that load_pg_gan_model is called only once, when the app first loads
+# Ensure that load_pg_gan_model is called only once, when the app first loads.
 @st.cache(allow_output_mutation=True, hash_funcs=TL_GAN_HASH_FUNCS)
 def load_pg_gan_model():
     """
     Create the tensorflow session.
     """
-    # Open a new TensorFlow session
+    # Open a new TensorFlow session.
     config = tf.ConfigProto(allow_soft_placement=True)
     session = tf.Session(config=config)
 
-    # Must have a default TensorFlow session established in order to initialize the GAN
+    # Must have a default TensorFlow session established in order to initialize the GAN.
     with session.as_default():
         # Read in either the GPU or the CPU version of the GAN
         with open(MODEL_FILE_GPU if USE_GPU else MODEL_FILE_CPU, 'rb') as f:
             G = pickle.load(f)
     return session, G
 
-# Ensure that load_tl_gan_model is called only once, when the app first loads
+# Ensure that load_tl_gan_model is called only once, when the app first loads.
 @st.cache(hash_funcs=TL_GAN_HASH_FUNCS)
 def load_tl_gan_model():
     """
@@ -117,13 +117,13 @@ def load_tl_gan_model():
     with open(FEATURE_DIRECTION_FILE, 'rb') as f:
         feature_direction_name = pickle.load(f)
 
-    # Pick apart the feature_direction_name data structure
+    # Pick apart the feature_direction_name data structure.
     feature_direction = feature_direction_name['direction']
     feature_names = feature_direction_name['name']
     num_feature = feature_direction.shape[1]
     feature_lock_status = np.zeros(num_feature).astype('bool')
 
-    # Rearrange feature directions using Shaobo's library function
+    # Rearrange feature directions using Shaobo's library function.
     feature_direction_disentangled = \
         feature_axis.disentangle_feature_axis_by_idx(
             feature_direction,
@@ -140,23 +140,23 @@ def get_random_features(feature_names, seed):
     return features
 
 # Hash the TensorFlow session, the pg-GAN model, and the TL-GAN model by id
-# to avoid expensive or illegal computations
+# to avoid expensive or illegal computations.
 @st.cache(show_spinner=False, hash_funcs=TL_GAN_HASH_FUNCS)
 def generate_image(session, pg_gan_model, tl_gan_model, features, feature_names):
     """
     Converts a feature vector into an image.
     """
-    # Create rescaled feature vector
+    # Create rescaled feature vector.
     feature_values = np.array([features[name] for name in feature_names])
     feature_values = (feature_values - 50) / 250
-    # Multiply by Shaobo's matrix to get the latent variables
+    # Multiply by Shaobo's matrix to get the latent variables.
     latents = np.dot(tl_gan_model, feature_values)
     latents = latents.reshape(1, -1)
     dummies = np.zeros([1] + pg_gan_model.input_shapes[1][1:])
-    # Feed the latent vector to the GAN in TensorFlow
+    # Feed the latent vector to the GAN in TensorFlow.
     with session.as_default():
         images = pg_gan_model.run(latents, dummies)
-    # Rescale and reorient the GAN's output to make an image
+    # Rescale and reorient the GAN's output to make an image.
     images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0),
                               0.0, 255.0).astype(np.uint8)  # [-1,1] => [0,255]
     if USE_GPU:
